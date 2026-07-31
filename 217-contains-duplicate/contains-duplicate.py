b9b980c88e190e970274920f25986612efab26ab
A-creator-2468 <veeramanojannamreddy@gmail.com>
2026-07-31 13:49:30 +0530

    Time: 10 ms (78.09%) | Memory: 32.4 MB (30.93%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,18 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]):
+        if len(set(nums))==len(nums):
+            return False
+        else:
+            return True    
+       
+
+
+        
+
+       
+        
+       
+
+       
+     
+        
