fc7ed67f6d749b5e0b55270d3765251cccf776a5
A-creator-2468 <veeramanojannamreddy@gmail.com>
2026-08-14 11:59:23 +0530

    Time: 27 ms (91.12%) | Memory: 21.9 MB (56.30%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,13 +1,16 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        csum=0
+        csum=0 #this is our prefixsum
         subcnt=0
-        seen={0:1}
+        seen={0:1} #hash map to store prefix sums found so far
         for i in nums:
-            csum+=i
+            csum+=i#compute prefix sum
+            #chech if req in prefixes so far
             req=csum-k
             if req in seen:
                 subcnt+=seen[req]
+                # add the number of times we seen that prefix 
+                #push the current prefix in the hash map
             seen[csum]=seen.get(csum,0)+1
         return subcnt        
        
