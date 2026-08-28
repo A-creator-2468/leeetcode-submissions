b1df9be6b83df1398ddf7653c11b45ebf0f5df06
A-creator-2468 <veeramanojannamreddy@gmail.com>
2026-08-28 21:58:47 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (19.73%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.py
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.py
@@ -1,9 +1,11 @@
 class Solution:
     def missingMultiple(self, nums: List[int], k: int) -> int:
-        nums_set = set(nums)
-        multiple = k
+        nums_set = {}
+        for num in nums:
+            nums_set[num]=nums_set.get(num,0)+1
+        multiple=k    
         
-        while multiple in nums_set:
+        while multiple in nums_set.keys():
             multiple += k
             
         return multiple
