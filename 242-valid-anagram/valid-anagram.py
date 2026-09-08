90847da1609e485278d728f3c91883f43132d185
A-creator-2468 <veeramanojannamreddy@gmail.com>
2026-09-08 10:53:01 +0530

    Time: 11 ms (77.28%) | Memory: 19.5 MB (29.47%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -4,18 +4,20 @@ class Solution:
         if len(s)!=len(t):
             return False
         for i in s:
-            if i in d.keys():
+            if i in d:
                 d[i]+=1
             else:
                 d[i]=1
         for i in t:
             if i not in d:
-               
                 return False
             d[i]-=1
             if d[i]==0:
                 del d[i]
-        return len(d)==0
+        return len(d)==0        
+                    
+
+       
                        
                     
            
