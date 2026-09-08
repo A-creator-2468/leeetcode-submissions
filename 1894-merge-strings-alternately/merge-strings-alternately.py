e492f7e53666b08d6e71c3353a0022e74a922d3e
A-creator-2468 <veeramanojannamreddy@gmail.com>
2026-09-08 11:52:46 +0530

    Time: 14 ms (73.08%) | Memory: 12.3 MB (58.53%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        result=[]
+        left=0
+        right=0
+        while left<len(word1) and right<len(word2):
+            result.append(word1[left])
+            result.append(word2[right])
+            left+=1
+            right+=1
+        if left<len(word1):
+            result.append(word1[left:])
+        if right<len(word2):
+            result.append(word2[right:])
+        return"".join(result)    
+
+
+
+       
+        
